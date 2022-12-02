b28e4d6e1fbbaa7b685e3bd51f2ded5e29b3b02a
Nohus <git@nohus.eu>
2022-12-02 06:21:46 +0000

    Day 1

--- src/main/kotlin/day01/Day1_2.kt
+++ src/main/kotlin/day01/Day1_2.kt
@@ -3,5 +3,9 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-
+    mutableListOf(0).also { elves ->
+        lines.forEach { line ->
+            if (line.isBlank()) elves += 0 else elves[elves.lastIndex] += line.toInt()
+        }
+    }.sorted().takeLast(3).sum()
 }
