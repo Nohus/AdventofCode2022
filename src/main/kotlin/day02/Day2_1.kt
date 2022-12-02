7efda2a39972fcedca7d94868c4ca4a11ad92d56
Nohus <git@nohus.eu>
2022-12-02 06:21:53 +0000

    Day 2

--- src/main/kotlin/day02/Day2_1.kt
+++ src/main/kotlin/day02/Day2_1.kt
@@ -3,5 +3,8 @@ package day02
 import solve
 
 fun main() = solve { lines ->
-
+    val win = mapOf('A' to 'Y', 'B' to 'Z', 'C' to 'X')
+    lines.sumOf { line ->
+        line[2].code - 'X'.code + 1 + if (win[line[0]] == line[2]) 6 else 0 + if (line[2] - line[0] == ('X' - 'A')) 3 else 0
+    }
 }
--- src/main/kotlin/day02/Day2_2.kt
+++ src/main/kotlin/day02/Day2_2.kt
@@ -3,5 +3,12 @@ package day02
 import solve
 
 fun main() = solve { lines ->
-
+    val win = mapOf('A' to 'Y', 'B' to 'Z', 'C' to 'X')
+    val draw = mapOf('A' to 'X', 'B' to 'Y', 'C' to 'Z')
+    val lose = mapOf('A' to 'Z', 'B' to 'X', 'C' to 'Y')
+    lines.sumOf { line ->
+        if (line[2] == 'Z') win[line[0]]!!.code - 'X'.code + 1 + 6
+        else if (line[2] == 'Y') draw[line[0]]!!.code - 'X'.code + 1 + 3
+        else lose[line[0]]!!.code - 'X'.code + 1
+    }
 }
