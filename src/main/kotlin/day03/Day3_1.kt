a3152f3f868793300297c0bc7711af2163d87ced
Nohus <git@nohus.eu>
2022-12-03 05:42:09 +0000

    Day 3

--- src/main/kotlin/day03/Day3_1.kt
+++ src/main/kotlin/day03/Day3_1.kt
@@ -3,5 +3,9 @@ package day03
 import solve
 
 fun main() = solve { lines ->
-
+    lines.sumOf { sack ->
+        val (a, b) = sack.chunked(sack.length / 2)
+        val item = a.first { it in b }
+        if (item.isUpperCase()) item.code - 'A'.code + 27 else item.code - 'a'.code + 1
+    }
 }
--- src/main/kotlin/day03/Day3_2.kt
+++ src/main/kotlin/day03/Day3_2.kt
@@ -3,5 +3,8 @@ package day03
 import solve
 
 fun main() = solve { lines ->
-
+    lines.chunked(3) { group ->
+        val item = group.joinToString("").first { item -> group.all { item in it } }
+        if (item.isUpperCase()) item.code - 'A'.code + 27 else item.code - 'a'.code + 1
+    }.sum()
 }
