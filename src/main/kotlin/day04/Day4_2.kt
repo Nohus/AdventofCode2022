43e53d95b00d4db72c347f67222117383364ad31
Nohus <git@nohus.eu>
2022-12-04 07:30:14 +0000

    Day 4

--- src/main/kotlin/day04/Day4_2.kt
+++ src/main/kotlin/day04/Day4_2.kt
@@ -3,5 +3,8 @@ package day04
 import solve
 
 fun main() = solve { lines ->
-
+    lines.count { line ->
+        val (a, b) = line.split(",").map { it.split("-").map { it.toInt() }.let { (a, b) -> (a..b) } }
+        (a.first in b) || (b.first in a)
+    }
 }
