0c419c2587154a24d4dae0400475a87b22ea61e7
Nohus <git@nohus.eu>
2022-12-05 06:13:34 +0000

    Day 5

--- src/main/kotlin/day05/Day5_1.kt
+++ src/main/kotlin/day05/Day5_1.kt
@@ -2,6 +2,21 @@ package day05
 
 import solve
 
-fun main() = solve { lines ->
-
+fun main() = solve(trim = false) { lines ->
+    val crates = (1..9).associateWith { emptyList<Char>() }.toMutableMap()
+    lines.filter { '[' in it }.forEach { line ->
+        for (stackIndex in 1..9) {
+            val lineIndex = 1 + (stackIndex - 1) * 4
+            if (line.lastIndex < lineIndex || line[lineIndex] == ' ') continue
+            crates[stackIndex] = listOf(line[lineIndex]) + crates.getValue(stackIndex)
+        }
+    }
+    lines.filter { "move" in it }.forEach { line ->
+        val (amount, from, to) = "\\d+".toRegex().findAll(line).map { it.value.toInt() }.toList()
+        repeat(amount) {
+            crates[to] = crates.getValue(to) + crates.getValue(from).last()
+            crates[from] = crates.getValue(from).dropLast(1)
+        }
+    }
+    crates.values.map { it.last() }.joinToString("")
 }
--- src/main/kotlin/day05/Day5_2.kt
+++ src/main/kotlin/day05/Day5_2.kt
@@ -2,6 +2,19 @@ package day05
 
 import solve
 
-fun main() = solve { lines ->
-
+fun main() = solve(trim = false) { lines ->
+    val crates = (1..9).associateWith { emptyList<Char>() }.toMutableMap()
+    lines.filter { '[' in it }.forEach { line ->
+        for (stackIndex in 1..9) {
+            val lineIndex = 1 + (stackIndex - 1) * 4
+            if (line.lastIndex < lineIndex || line[lineIndex] == ' ') continue
+            crates[stackIndex] = listOf(line[lineIndex]) + crates.getValue(stackIndex)
+        }
+    }
+    lines.filter { "move" in it }.forEach { line ->
+        val (amount, from, to) = "\\d+".toRegex().findAll(line).map { it.value.toInt() }.toList()
+        crates[to] = crates.getValue(to) + crates.getValue(from).takeLast(amount)
+        crates[from] = crates.getValue(from).dropLast(amount)
+    }
+    crates.values.map { it.last() }.joinToString("")
 }
