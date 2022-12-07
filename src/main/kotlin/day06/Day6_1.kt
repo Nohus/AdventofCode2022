82f99dd77a0c739c055316cad919931159804a5d
Nohus <git@nohus.eu>
2022-12-07 04:55:47 +0000

    Day 6

--- src/main/kotlin/day06/Day6_1.kt
+++ src/main/kotlin/day06/Day6_1.kt
@@ -3,5 +3,5 @@ package day06
 import solve
 
 fun main() = solve { lines ->
-
+    lines.first().windowed(4).indexOfFirst { it.toSet().size == 4 } + 4
 }
--- src/main/kotlin/day06/Day6_2.kt
+++ src/main/kotlin/day06/Day6_2.kt
@@ -3,5 +3,5 @@ package day06
 import solve
 
 fun main() = solve { lines ->
-
+    lines.first().windowed(14).indexOfFirst { it.toSet().size == 14 } + 14
 }
