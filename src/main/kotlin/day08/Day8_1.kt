845828c87ff505ef5fe34983ba1957a1d97457c7
Nohus <git@nohus.eu>
2022-12-08 06:05:59 +0000

    Day 8

--- src/main/kotlin/day08/Day8_1.kt
+++ src/main/kotlin/day08/Day8_1.kt
@@ -1,7 +1,22 @@
 package day08
 
 import solve
+import utils.Point
 
 fun main() = solve { lines ->
-
+    val trees = mutableMapOf<Point, Int>()
+    lines.forEachIndexed { y, row ->
+        row.forEachIndexed { x, height ->
+            trees[Point(x, y)] = height.digitToInt()
+        }
+    }
+    val (maxX, maxY) = trees.keys.last()
+    trees.count { (point, height) ->
+        if (point.x == 0 || point.y == 0 || point.x == maxX || point.y == maxY) return@count true
+        if ((0..<point.x).all { trees[Point(it, point.y)]!! < height }) return@count true
+        if (((point.x + 1)..maxX).all { trees[Point(it, point.y)]!! < height }) return@count true
+        if ((0..<point.y).all { trees[Point(point.x, it)]!! < height }) return@count true
+        if (((point.y + 1)..maxY).all { trees[Point(point.x, it)]!! < height }) return@count true
+        false
+    }
 }
--- src/main/kotlin/day08/Day8_2.kt
+++ src/main/kotlin/day08/Day8_2.kt
@@ -1,7 +1,25 @@
 package day08
 
 import solve
+import utils.Point
 
 fun main() = solve { lines ->
-
+    val trees = mutableMapOf<Point, Int>()
+    lines.forEachIndexed { y, row ->
+        row.forEachIndexed { x, height ->
+            trees[Point(x, y)] = height.digitToInt()
+        }
+    }
+    val (maxX, maxY) = trees.keys.last()
+    trees.maxOf { (point, height) ->
+        var left = ((point.x - 1) downTo 0).takeWhile { trees[Point(it, point.y)]!! < height }.size
+        if ((0 until point.x).count() > left) left++
+        var right = ((point.x + 1)..maxX).takeWhile { trees[Point(it, point.y)]!! < height }.size
+        if (((point.x + 1)..maxX).count() > right) right++
+        var up = ((point.y - 1) downTo 0).takeWhile { trees[Point(point.x, it)]!! < height }.size
+        if ((0 until point.y).count() > up) up++
+        var down = ((point.y + 1)..maxY).takeWhile { trees[Point(point.x, it)]!! < height }.size
+        if (((point.y + 1)..maxY).count() > down) down++
+        left * right * up * down
+    }
 }
