82493014ca11b9ec664f4577169759a09a0e8832
Nohus <git@nohus.eu>
2022-12-10 05:33:44 +0000

    Day 10

--- src/main/kotlin/day10/Day10_1.kt
+++ src/main/kotlin/day10/Day10_1.kt
@@ -3,5 +3,21 @@ package day10
 import solve
 
 fun main() = solve { lines ->
+    var x = 1
+    var cycle = 1
+    var sum = 0
 
+    fun processCycle() {
+        if (cycle % 40 == 20) sum += cycle * x
+        cycle++
+    }
+
+    lines.forEach { line ->
+        if (line == "noop") processCycle()
+        else {
+            processCycle(); processCycle()
+            x += line.substringAfter(" ").toInt()
+        }
+    }
+    sum
 }
--- src/main/kotlin/day10/Day10_2.kt
+++ src/main/kotlin/day10/Day10_2.kt
@@ -1,7 +1,28 @@
 package day10
 
 import solve
+import utils.Point
+import utils.printArea
 
 fun main() = solve { lines ->
+    var x = 1
+    var cycle = 1
+    val screen = mutableMapOf<Point, Boolean>()
 
+    fun processCycle() {
+        val screenX = (cycle - 1) % 40
+        if (screenX in (x - 1)..(x + 1)) {
+            screen[Point(screenX, (cycle - 1) / 40)] = true
+        }
+        cycle++
+    }
+
+    lines.forEach { line ->
+        if (line == "noop") processCycle()
+        else {
+            processCycle(); processCycle()
+            x += line.substringAfter(" ").toInt()
+        }
+    }
+    screen.printArea()
 }
