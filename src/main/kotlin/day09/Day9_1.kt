b6fc0cd89f415c7f5a5b3a7ef2db24686a36b3c2
Nohus <git@nohus.eu>
2022-12-10 05:33:37 +0000

    Day 9

--- src/main/kotlin/day09/Day9_1.kt
+++ src/main/kotlin/day09/Day9_1.kt
@@ -1,7 +1,30 @@
 package day09
 
 import solve
+import utils.Direction
+import utils.Direction.*
+import utils.Point
+import kotlin.math.sign
 
 fun main() = solve { lines ->
-
+    var head = Point.ORIGIN
+    var tail = Point.ORIGIN
+    val visited = mutableSetOf(Point.ORIGIN)
+    lines.forEach { line ->
+        val direction: Direction = when (line.substringBefore(" ")) {
+            "U" -> NORTH
+            "L" -> WEST
+            "R" -> EAST
+            "D" -> SOUTH
+            else -> error("")
+        }
+        repeat(line.substringAfter(" ").toInt()) {
+            head = head.move(direction)
+            if (tail !in head.getAdjacent()) {
+                tail = Point(tail.x + (head.x - tail.x).sign, tail.y + (head.y - tail.y).sign)
+                visited += tail
+            }
+        }
+    }
+    visited.size
 }
--- src/main/kotlin/day09/Day9_2.kt
+++ src/main/kotlin/day09/Day9_2.kt
@@ -1,7 +1,34 @@
 package day09
 
 import solve
+import utils.Direction
+import utils.Direction.*
+import utils.Point
+import kotlin.math.sign
 
 fun main() = solve { lines ->
-
+    val knots = MutableList(10) { Point.ORIGIN }
+    val visited = mutableSetOf(Point.ORIGIN)
+    lines.forEach { line ->
+        val direction: Direction = when (line.substringBefore(" ")) {
+            "U" -> NORTH
+            "L" -> WEST
+            "R" -> EAST
+            "D" -> SOUTH
+            else -> error("")
+        }
+        repeat(line.substringAfter(" ").toInt()) {
+            knots[0] = knots[0].move(direction)
+            knots.drop(1).indices.forEach { index ->
+                val head = knots[index]
+                var tail = knots[index + 1]
+                if (tail !in head.getAdjacent()) {
+                    tail = Point(tail.x + (head.x - tail.x).sign, tail.y + (head.y - tail.y).sign)
+                    visited += knots.last()
+                }
+                knots[index + 1] = tail
+            }
+        }
+    }
+    visited.size
 }
